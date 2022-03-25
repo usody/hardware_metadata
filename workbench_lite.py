5d8827ed393cb0e41f65e0927f1e3410ea5e0cd5
nad <jnadeu@gmail.com>
2022-03-25 14:46:51 +0100

    add lshw package to get hw data

--- workbench_lite.py
+++ workbench_lite.py
@@ -15,7 +15,6 @@ class WorkbenchLite:
         if os.geteuid() != 0:
             raise EnvironmentError('[ERROR] Execute WorkbenchLite as root / sudo. \r')
         self.type = 'Snapshot'
-        self.timestamp = datetime.now()
         self.snapshot_uuid = uuid.uuid4()
         self.software = 'WorkbenchLite'
         self.version = '2022.03'
@@ -48,7 +47,8 @@ class WorkbenchLite:
         proc = subprocess.Popen(cmd_lsblk, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output_lsblk, errors_lsblk = proc.communicate()
         proc.wait()
-        disk_info = json.loads(str(output_lsblk.decode('utf8')))
+        disk_info = json.loads(output_lsblk)
+
         smart_data = []
         if proc.returncode >= 0:
             for disk in disk_info['blockdevices']:
@@ -57,13 +57,22 @@ class WorkbenchLite:
                     proc2 = subprocess.Popen(smart_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                     output_smart, errors_smart = proc2.communicate()
                     proc2.wait()
-                    if proc2.returncode < 0:
-                        smart_data.append(str(errors_smart.decode('utf8')))
-                    else:
-                        smart_info = json.loads(str(output_smart.decode('utf8')))
+                    if proc2.returncode >= 0:
+                        smart_info = json.loads(output_smart)
                         smart_data.append(smart_info)
+                        # TODO fix if json loads fails
+                        # smart_data.append(str(output_smart))
+                    elif "UNRECOGNIZED OPTION" in output_smart.decode('utf8'):
+                        cmd2 = ["smartctl -x /dev/" + disk['kname']]
+                        proc2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                        output_smart2, errors2 = proc2.communicate()
+                        proc2.wait()
+                        smart_data.append(output_smart2.decode('utf8'))
+                    else:
+                        smart_data.append(errors_smart.decode('utf8'))
+
         else:
-            print('[ERROR] SMARTCTL failed execution with output: ', errors_lsblk, '\r')
+            print('[ERROR] Getting disks information failed with output: ', errors_lsblk, '\r')
             return [errors_lsblk]
         print('[INFO] SMART successfully completed. \r')
         return smart_data
@@ -77,26 +86,48 @@ class WorkbenchLite:
 
         if proc.returncode < 0:
             print('[ERROR] HWINFO failed execution with output: ', hwinfo_errors, '\r')
-            return str(hwinfo_errors.decode('utf8'))
+            return hwinfo_errors.decode('utf8')
 
-        hwinfo_data = str(hwinfo_output.decode('utf8'))
+        hwinfo_data = hwinfo_output.decode('utf8')
 
         print('[INFO] HWINFO successfully completed. \r')
         return hwinfo_data
+
+    def get_lshw_data(self):
+        """Get DMI table information using dmidecode command."""
+        lshw_command = ['lshw -json']
+        proc = subprocess.Popen(lshw_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lshw_output, lshw_errors = proc.communicate()
+        proc.wait()
+
+        if proc.returncode < 0:
+            print('[ERROR] HWINFO failed execution with output: ', lshw_errors, '\r')
+            return lshw_errors.decode('utf8')
+
+        lshw_data = json.loads(lshw_output)
+
+        print('[INFO] LSHW successfully completed. \r')
+        return lshw_data
 
     def generate_snapshot(self):
         """ Getting hardware data and generate snapshot file (json)."""
         # Generate WB ID base on snapshot uuid value
         wbid = self.generate_wbid(self.snapshot_uuid)
-        print('[INFO] | WBID | =', wbid, '\r')
+        print('[INFO] | WBID | =>', wbid, '\r')
 
+        # Get hardware data
+        lshw_data = self.get_lshw_data()
         dmi_data = self.get_dmi_data()
-        smart_data = self.get_smart_data()
         hwinfo_data = self.get_hwinfo_data()
-        snapshot_data = {'dmidecode': dmi_data, 'hwinfo': hwinfo_data, 'smart': smart_data}
+        smart_data = self.get_smart_data()
+        snapshot_data = {'lshw': lshw_data, 'dmidecode': dmi_data, 'hwinfo': hwinfo_data, 'smart': smart_data}
 
+        # Generate timestamp
+        timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+        # Generate and save snapshot
         snapshot = {
-            'timestamp': str(self.timestamp),
+            'timestamp': str(timestamp),
             'type': 'Snapshot',
             'uuid': str(self.snapshot_uuid),
             'wbid': wbid,
@@ -105,18 +136,21 @@ class WorkbenchLite:
             'data': snapshot_data
         }
 
-        # TODO change snapshot file name to {date}_{wbid}_WBv{version}...json
-        with open('snapshot.json', 'w') as file:
-            json.dump(snapshot, file)
+        json_file = '{date}_{wbid}_WBv{version}_snapshot.json'.format(
+                                                                date=timestamp,
+                                                                wbid=wbid, version=str(self.version))
+        with open(json_file, 'w') as file:
+            json.dump(snapshot, file, indent=2)
 
         print('[INFO] Snapshot JSON successfully saved. \r')
 
 
 if __name__ == '__main__':
     workbench_lite = WorkbenchLite()
-    print('[INFO] Starting WBv14! \r')
+    print('[INFO] ---- Starting Workbench ---- \r')
+    print('[VERSION]', workbench_lite.version, '\r')
     workbench_lite.generate_snapshot()
-    print('[INFO] Finished WBv14! \r')
+    print('[INFO] ---- Workbench finished ---- \r')
 
 
 # Funciton to convert an int32 to an alphanumeric ID of 6 characters
