906541d024ff051bb2ac10b4567795b546e746c8
nad <jnadeu@gmail.com>
2022-06-02 11:47:49 +0200

    change paths on decouple settings

--- workbench_settings.py
+++ workbench_settings.py
@@ -3,11 +3,11 @@ from decouple import AutoConfig
 
 class WorkbenchSettings:
     # Path where find settings.ini file
-    config = AutoConfig(search_path='/wb/settings/')
+    config = AutoConfig(search_path='/wb_settings/')
 
     # Env variables for DH parameters
     DH_TOKEN = config('DH_TOKEN', default='')
     DH_URL = config('DH_URL', default='')
 
     # Path where store snapshot files
-    WB_SNAPSHOT_PATH = config('WB_SNAPSHOT_PATH', default='/wb/snapshots/')
+    WB_SNAPSHOT_PATH = config('WB_SNAPSHOT_PATH', default='/wb_snapshots/')
