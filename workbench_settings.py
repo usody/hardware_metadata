34aa82a2f79c4cfd4fb4d1e3fc5a6f730fe999b3
nad <jnadeu@gmail.com>
2022-06-04 11:58:34 +0200

    minor fix in persistence path

--- workbench_settings.py
+++ workbench_settings.py
@@ -3,11 +3,11 @@ from decouple import AutoConfig
 
 class WorkbenchSettings:
     # Path where find settings.ini file
-    config = AutoConfig(search_path='/wb_settings/')
+    config = AutoConfig(search_path='/mnt/wb_settings/')
 
     # Env variables for DH parameters
     DH_TOKEN = config('DH_TOKEN', default='', cast=str)
     DH_URL = config('DH_URL', default='', cast=str)
 
     # Path where store snapshot files
-    WB_SNAPSHOT_PATH = config('WB_SNAPSHOT_PATH', default='/wb_snapshots/', cast=str)
+    WB_SNAPSHOT_PATH = config('WB_SNAPSHOT_PATH', default='/mnt/wb_snapshots/', cast=str)
