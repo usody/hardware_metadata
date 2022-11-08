9658436f7e4f5e4a705f84eb02dad945c3084453
nad <jnadeu@gmail.com>
2022-11-08 12:34:56 +0100

    new variable in the settings, VERSION (rel #3998)

---
+++ workbench_utils.py
@@ -0,0 +1,35 @@
+import socket
+
+from decouple import AutoConfig
+
+
+class WorkbenchSettings:
+    # Path where find settings.ini file
+    config = AutoConfig(search_path='/mnt/wb_settings/')
+
+    # Env variables for DH parameters
+    DH_TOKEN = config('DH_TOKEN', default='', cast=str)
+    DH_URL = config('DH_URL', default='', cast=str)
+    VERSION = config('VERSION', default='', cast=str)
+
+    # Path where store snapshot files
+    SNAPSHOT_PATH = config('SNAPSHOT_PATH', default='/mnt/snapshots/', cast=str)
+
+
+class WorkbenchUtils:
+    """ Collection of useful functions for the working of the tool. """
+
+    def internet(host="8.8.8.8", port=53, timeout=3):
+        """
+        Host: 8.8.8.8 (google-public-dns-a.google.com)
+        OpenPort: 53/tcp
+        Service: domain (DNS/TCP)
+        Source: https://stackoverflow.com/a/33117579
+        """
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            return True
+        except socket.error as ex:
+            print('[WARNING] No Internet.', ex.strerror)
+            #return False
